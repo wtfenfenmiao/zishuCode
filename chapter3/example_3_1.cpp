f729664a370ebca8efbf9b2c2864cd2a80cc436a
wtfenfenmiao <wtbupt@outlook.com>
2018-08-01 09:27:59 +0800

    20180731

--- chapter3/example_3_1.cpp
+++ chapter3/example_3_1.cpp
@@ -1,1 +1,20 @@
-
+#include<stdio.h>
+int main()
+{
+    freopen("example_3_1_in.txt","r",stdin);
+    freopen("example_3_1_out.txt","w",stdout);
+    char c;
+    bool flag=true;
+    //这个while里的赋值可以学习一下
+    while((c=getchar())!=EOF)     //不能用scanf了，scanf遇到空格和回车会停下。还可以用fgetc(fin)
+    {
+        if(c=='"')
+        {
+          c=flag?'@':'$';
+          flag=!flag;
+        }
+        printf("%c",c);
+    }
+    printf("\n");
+    return 0;
+}
